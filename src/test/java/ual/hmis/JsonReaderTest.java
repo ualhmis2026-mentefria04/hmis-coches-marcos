87b306b055116c236d63353a3207b54abdb5f2f7
Marcos Escanez Clemente <mec700@inlumine.ual.es>
2026-04-09 14:09:30 +0200

    primer commit

---
+++ src/test/java/ual/hmis/JsonReaderTest.java
@@ -0,0 +1,28 @@
+package ual.hmis;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JsonReaderTest {
+
+	@Test
+	void testLeerCochesJSON() {
+		String ruta = "data/coches.json";
+		Coche [] coches = JsonReader.leerCochesJSON(ruta);
+		assertEquals (4, coches.length);
+	}
+
+	@Test
+	void testLeerCochesJSONprimero() {
+		String ruta = "data/coches.json";
+		Coche primero = new Coche ("Toyota", "Corolla", 2022, 22000);
+		Coche [] coches = JsonReader.leerCochesJSON(ruta);
+		assertEquals(primero, coches[0]);
+		assertTrue (primero.equals(coches[0]));
+		assertTrue (coches[0].equals(primero));
+	}
+
+
+}
